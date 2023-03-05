07e369ef94145df90557e16824551f9f1bb53688
Tony Abou-Zeidan <tony.azp25@gmail.com>
2023-03-05 15:16:22 -0500

    Basic model of the project.
    I verify that the entire group was present for this basic structure about the project.

--- src/main/java/fakeamazon/bookstore/demo/controller/UserController.java
+++ src/main/java/fakeamazon/bookstore/demo/controller/UserController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/owner")
-public class BookOwnerController {
+@RequestMapping("/user")
+public class UserController {
 
-
+    
 }
--- src/main/java/fakeamazon/bookstore/demo/model/Book.java
+++ src/main/java/fakeamazon/bookstore/demo/model/Book.java
@@ -1,4 +1,18 @@
 package fakeamazon.bookstore.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
 public class Book {
+
+    /**
+     * Books need the following items:
+     * - id: long
+     * - quantity: int
+     * - isbn: string
+     * - picture: string (base64 string)
+     * - description: string
+     * - publisher: string
+     */
+
 }
