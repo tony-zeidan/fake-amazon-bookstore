b18f83224d9db14879539b2fb6e8a7273c3b097b
Tony Abou Zeidan <tony.azp25@gmail.com>
2023-03-06 14:11:23 -0500

    Getting ready to do upload.

--- src/main/java/fakeamazon/bookstore/demo/controller/BookOwnerRestController.java
+++ src/main/java/fakeamazon/bookstore/demo/controller/BookOwnerRestController.java
@@ -8,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RestController
 public class BookOwnerRestController {
 
