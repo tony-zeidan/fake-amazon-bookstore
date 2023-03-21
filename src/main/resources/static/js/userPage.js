ea15f34ca7371256d912130509afb6877e05b46f
bruce <bruceliu1107@gmail.com>
2023-03-21 14:05:21 -0400

    progress with userPage div creation

--- src/main/resources/static/js/userPage.js
+++ src/main/resources/static/js/userPage.js
@@ -13,6 +13,7 @@ const fetchBooksPages = () => {
         success: function(data, status, xhrObject)
         {
             displayBooks(data['books']);
+
         },
         error: function(jqXHR, textStatus, errorThrown) {
             alert(errorThrown);
@@ -22,4 +23,30 @@ const fetchBooksPages = () => {
 
 const displayBooks = (bookData) => {
     console.log(bookData);
+    const booksContainer = $(document.querySelector("#user-books-container"));
+    bookData.forEach((data) => {
+        const title = data.name
+        const picture = data.picture
+        let userBookDiv = $(document.createElement("div"))
+        userBookDiv.attr('class', 'user-book-container')
+
+        let userBookTitleDiv = $(document.createElement("div"))
+        userBookDiv.attr('class', 'user-book-title-container')
+
+        let titleText = $(document.createElement("p"))
+        titleText.text(title);
+        titleText.attr('class', 'title-text')
+        userBookTitleDiv.append(titleText);
+
+        let pictureDiv = $(document.createElement("div"))
+        pictureDiv.attr('class', 'user-picture-container')
+        let img = $(document.createElement("img"));
+        img.attr('src', picture);
+        img.attr('class', 'user-book-image')
+        pictureDiv.append(img)
+
+        userBookDiv.append(userBookTitleDiv)
+        userBookDiv.append(pictureDiv)
+        booksContainer.append(userBookDiv);
+    })
 }
