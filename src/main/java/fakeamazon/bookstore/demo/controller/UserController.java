3df338b253877492cb3047596d06288efec9bb5a
Mark Johnson <xfoundry27@yahoo.ca>
2023-03-20 21:58:33 -0400

    Implement user registration system

--- src/main/java/fakeamazon/bookstore/demo/controller/UserController.java
+++ src/main/java/fakeamazon/bookstore/demo/controller/UserController.java
@@ -1,17 +1,51 @@
 package fakeamazon.bookstore.demo.controller;
 
+import fakeamazon.bookstore.demo.dto.UserDTO;
+import fakeamazon.bookstore.demo.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    public UserController() {
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/registration")
+    public String registerUser(Model model) {
+        UserDTO userDto = new UserDTO();
+        model.addAttribute("user", userDto);
+        return "userregistration";
+    }
+
+    @PostMapping("/registration")
+    public String registerUserAccount(
+            @ModelAttribute("user") @Valid UserDTO userDto,
+            BindingResult bindingResult) {
+        try {
+            final UserDetails user = this.userService.registerNewUser(userDto);
+        } catch (final IllegalArgumentException e) {
+            bindingResult.reject("user.registration.invalid", e.getMessage());
+            return "userregistration";
+        }
+        return "redirect:/login";
     }
 
     @GetMapping("")
---
+++ src/main/java/fakeamazon/bookstore/demo/dto/UserDTO.java
@@ -0,0 +1,33 @@
+package fakeamazon.bookstore.demo.dto;
+
+public class UserDTO {
+    private String username;
+
+    private String password;
+
+    private String matchingPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
+}
