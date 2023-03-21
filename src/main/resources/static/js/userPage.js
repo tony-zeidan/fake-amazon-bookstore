a1a2fd35bd0cd92a5ae87662d933277536a78457
bruce <bruceliu1107@gmail.com>
2023-03-21 18:31:11 -0400

    filter and page feature on the user page

--- src/main/resources/static/js/userPage.js
+++ src/main/resources/static/js/userPage.js
@@ -1,28 +1,39 @@
-$(document).ready(function(){
-    console.log("user page ready")
-    fetchBooksPages();
-});
-
+const page = {
+    currentPage:0,
+    totalPages:0
+}
 const fetchBooksPages = () => {
-    console.log("fetching books")
-    const url = new URL("http://localhost:8080/getAllBooksPage");
+    const nameFilter = document.querySelector("#userNameFilter");
+    const isbnFilter = document.querySelector("#userIsbnFilter");
+    const descriptionFilter = document.querySelector("#userDescriptionFilter");
+    const publisherFilter = document.querySelector("#userPublisherFilter");
+
+    let url = new URL("localhost:8080/getbooks");
+    if (nameFilter.value) url.searchParams.set("name", nameFilter.value);
+    if (isbnFilter.value) url.searchParams.set("isbn", isbnFilter.value);
+    if (descriptionFilter.value) url.searchParams.set("description", descriptionFilter.value);
+    if (publisherFilter.value) url.searchParams.set("publisher", publisherFilter.value);
+    if (page.currentPage) url.searchParams.set("page", page.currentPage);
+    url.searchParams.set('size', "4");
     $.ajax({
         type: "get",
-        url: url,
+        url: "/bookstore/getbooks?"+url.searchParams.toString(),
         dataType: 'json',
         success: function(data, status, xhrObject)
         {
+            page.currentPage = data["currentPage"];
+            page.totalPages = data["totalPages"];
             displayBooks(data['books']);
-
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function(jqXHR, textStatus, errorThrown)
+        {
             alert(errorThrown);
         }
     });
 }
 
 const displayBooks = (bookData) => {
-    console.log(bookData);
+    $("#user-books-container").empty()
     const booksContainer = $(document.querySelector("#user-books-container"));
     bookData.forEach((data) => {
         const title = data.name
@@ -44,6 +55,9 @@ const displayBooks = (bookData) => {
         img.attr('class', 'user-book-image')
         pictureDiv.append(img)
 
+        let cartActionDiv = $(document.createElement("div"))
+        cartActionDiv.attr('class', 'cart-selector')
+
         let quantityInputDiv = $(document.createElement("div"))
         quantityInputDiv.attr('class', 'quantity-input-container')
 
@@ -52,18 +66,75 @@ const displayBooks = (bookData) => {
 
         let quantityInput = $(document.createElement("input"))
         quantityInput.attr("type", 'number')
-        quantityInput.attr('value', 0)
+        quantityInput.attr('value', "1")
+        quantityInput.attr("min", "1")
+        quantityInput.attr("max", "10")
+        quantityInput.attr('class', 'cart-selector-select')
 
         quantityInputDiv.append(quantityLabel, quantityInput)
 
         let addCartButton = $(document.createElement("button"))
-        addCartButton.attr('class', 'add-to-cart-button')
+        addCartButton.attr('class', 'add-to-cart-button cart-selector-btn')
         addCartButton.text("add to cart")
+        addCartButton.click(() => {
+            handleAddToCart(data.id, quantityInput.val())
+        })
 
+
+        cartActionDiv.append(quantityInputDiv);
+        cartActionDiv.append(addCartButton);
         userBookDiv.append(userBookTitleDiv)
         userBookDiv.append(pictureDiv)
-        userBookDiv.append(quantityInputDiv)
-        userBookDiv.append(addCartButton)
+        userBookDiv.append(cartActionDiv)
         booksContainer.append(userBookDiv);
     })
+}
+
+$(document).ready(function() {
+    fetchBooksPages();
+    console.log(page.currentPage)
+    $("#user-search-button").click(() => {
+        page.currentPage = 0;
+        fetchBooksPages();
+    });
+
+    const nextButton = $("#nextButton");
+    nextButton.click( () => {
+        if (page.currentPage+1 === page.totalPages) return;
+        page.currentPage+=1;
+        fetchBooksPages();
+    });
+
+
+    const prevButton = $("#prevButton");
+    prevButton.click( () => {
+        if (page.currentPage === 0) return;
+        page.currentPage-=1;
+        fetchBooksPages();
+    });
+});
+
+/**
+ * Handles the adding of an item to the cart and the server response.
+ *
+ * @param book_id   The ID of the specific book entity to add
+ * @param quantity  The initial quantity of this book in the cart
+ */
+function handleAddToCart (book_id, quantity) {
+    $.ajax({
+        url: '/useractions/addtocart',
+        type: 'POST',
+        data: JSON.stringify({
+            'id': book_id,
+            'quantity': quantity
+        }),
+        success: function () {
+            alert("The book was added successfully!");
+        },
+        error: function() {
+            alert("There was an error in processing your action (id="+book_id+", count="+quantity+")");
+        },
+        processData: false,
+        contentType: 'application/json; charset=utf-8',
+    });
 }
