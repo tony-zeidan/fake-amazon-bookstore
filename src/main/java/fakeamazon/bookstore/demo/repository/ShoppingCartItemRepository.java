fbfeb8721ca24a07435a33539f8c16a2dc180489
Tony Abou Zeidan <tony.azp25@gmail.com>
2023-03-22 08:59:06 -0400

    Repository changes - actually made use of the new repository functions in the cart services.

--- src/main/java/fakeamazon/bookstore/demo/repository/ShoppingCartItemRepository.java
+++ src/main/java/fakeamazon/bookstore/demo/repository/ShoppingCartItemRepository.java
@@ -1,5 +1,7 @@
 package fakeamazon.bookstore.demo.repository;
 
+import fakeamazon.bookstore.demo.model.Book;
+import fakeamazon.bookstore.demo.model.Customer;
 import fakeamazon.bookstore.demo.model.ShoppingCartItem;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,4 +10,5 @@ public interface ShoppingCartItemRepository extends CrudRepository<ShoppingCartI
     ShoppingCartItem findById(long id);
     ShoppingCartItem findShoppingCartItemByBook_IdAndCustomer_Username(long book_Id, String customer_username);
     void deleteShoppingCartItemByBook_IdAndCustomer_Username(long book_Id, String customer_username);
+    void deleteAllByCustomer(Customer customer);
 }
--- src/main/java/fakeamazon/bookstore/demo/services/ShoppingCartService.java
+++ src/main/java/fakeamazon/bookstore/demo/services/ShoppingCartService.java
@@ -33,11 +33,12 @@ public class ShoppingCartService {
         this.shoppingCartItemRepo = shoppingCartItemRepo;
     }
 
-    public Optional<ShoppingCartItem> fromTemplate(BookQuantityTemplate template) {
+    public Optional<ShoppingCartItem> fromTemplate(Customer customer, BookQuantityTemplate template) {
         Optional<Book> book = bookRepo.findById(template.getId());
         ShoppingCartItem itemConcrete = null;
         if (book.isPresent()) {
             itemConcrete = new ShoppingCartItem();
+            itemConcrete.setCustomer(customer);
             itemConcrete.setBook(book.get());
             itemConcrete.setQuantity(template.getQuantity());
         }
@@ -47,32 +48,23 @@ public class ShoppingCartService {
     public Optional<ShoppingCartItem> addToCart(Authentication auth, BookQuantityTemplate template) {
 
         Customer customer = detailsService.getCustomerDetails(auth);
-        Optional<ShoppingCartItem> itemMade = fromTemplate(template);
+        Optional<ShoppingCartItem> itemMade = fromTemplate(customer, template);
 
         if (itemMade.isPresent()) {
-            ShoppingCartItem item = null;
             ShoppingCartItem itemToFind = itemMade.get();
-            boolean found = false;
 
-            // look for a duplicate book for adding collisions
-            for (ShoppingCartItem it: customer.getCart()) {
-
-                // if a duplicate book is found, simply add to the quantity
-                if (it.getBook().equals(itemToFind.getBook())) {
-                    found = true;
-                    it.setQuantity(it.getQuantity()+itemToFind.getQuantity());
-                    shoppingCartItemRepo.save(it);
-                    break;
-                }
-            }
-
-            if (!found) {
-                itemToFind.setCustomer(customer);
+            ShoppingCartItem item = shoppingCartItemRepo.findShoppingCartItemByBook_IdAndCustomer_Username(template.getId(), customer.getUsername());
+            if (item != null) {
+                item.setQuantity(item.getQuantity()+itemToFind.getQuantity());
+                shoppingCartItemRepo.save(item);
+                detailsService.saveCustomer(customer);
+                return Optional.empty();
+            } else {
                 customer.getCart().add(itemToFind);
-                item = shoppingCartItemRepo.save(itemToFind);
+                shoppingCartItemRepo.save(itemToFind);
+                detailsService.saveCustomer(customer);
+                return Optional.of(itemToFind);
             }
-            detailsService.saveCustomer(customer);
-            return Optional.ofNullable(item);
         } else {
             throw new ShoppingCartAddException(template.getId());
         }
@@ -80,45 +72,39 @@ public class ShoppingCartService {
 
     public Optional<ShoppingCartItem> changeCartAmount(Authentication auth, BookQuantityTemplate template) {
         Customer customer = detailsService.getCustomerDetails(auth);
-        ShoppingCartItem found = null;
-        ShoppingCartItem rm = null;
-        for (ShoppingCartItem item : customer.getCart()) {
-            if (item.getBook().getId() == template.getId()) {
-                found = item;
-                if (item.getQuantity() < Math.abs(template.getQuantity())) {
-                    if (template.getQuantity() < 0) {
-                        throw new ShoppingCartEditException(item.getQuantity(), template.getQuantity());
-                    } else {
-                        item.setQuantity(item.getQuantity() + template.getQuantity());
-                        shoppingCartItemRepo.save(item);
-                    }
-                    break;
+        ShoppingCartItem item = shoppingCartItemRepo.findShoppingCartItemByBook_IdAndCustomer_Username(template.getId(), customer.getUsername());
 
-                } else if (item.getQuantity() == Math.abs(template.getQuantity())) {
-                    if (template.getQuantity() < 0) {
-                        // delete fully
-                        rm = item;
-                    } else {
-                        item.setQuantity(item.getQuantity() + template.getQuantity());
-                        shoppingCartItemRepo.save(item);
-                    }
-                    break;
-
-                } else if (item.getQuantity() > Math.abs(template.getQuantity())) {
+        if (item != null) {
+            if (item.getQuantity() < Math.abs(template.getQuantity())) {
+                if (template.getQuantity() < 0) {
+                    throw new ShoppingCartEditException(item.getQuantity(), template.getQuantity());
+                } else {
                     item.setQuantity(item.getQuantity() + template.getQuantity());
                     shoppingCartItemRepo.save(item);
-                    break;
+                    detailsService.saveCustomer(customer);
                 }
-                break;
+
+            } else if (item.getQuantity() == Math.abs(template.getQuantity())) {
+                if (template.getQuantity() < 0) {
+                    // delete fully
+                    customer.getCart().remove(item);
+                    shoppingCartItemRepo.delete(item);
+                    detailsService.saveCustomer(customer);
+                } else {
+                    item.setQuantity(item.getQuantity() + template.getQuantity());
+                    shoppingCartItemRepo.save(item);
+                    detailsService.saveCustomer(customer);
+                }
+
+            } else if (item.getQuantity() > Math.abs(template.getQuantity())) {
+                item.setQuantity(item.getQuantity() + template.getQuantity());
+                shoppingCartItemRepo.save(item);
+                detailsService.saveCustomer(customer);
             }
+        } else {
+            return Optional.empty();
         }
-        // the item is to be deleted fully
-        if (rm != null) {
-            customer.getCart().remove(rm);
-            shoppingCartItemRepo.delete(rm);
-        }
-        detailsService.saveCustomer(customer);
-        return Optional.ofNullable(found);
+        return Optional.of(item);
     }
 
 
