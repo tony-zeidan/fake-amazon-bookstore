c7063dfb54993a853a4dc6a63a2bd835e28b77a1
AnthonyDooley <anthonylouisdooley@gmail.com>
2023-03-25 19:11:06 -0400

    changes

--- src/main/java/fakeamazon/bookstore/demo/services/BookRepoService.java
+++ src/main/java/fakeamazon/bookstore/demo/services/BookRepoService.java
@@ -3,7 +3,12 @@ package fakeamazon.bookstore.demo.services;
 import fakeamazon.bookstore.demo.model.Book;
 import fakeamazon.bookstore.demo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class BookRepoService {
@@ -23,4 +28,15 @@ public class BookRepoService {
         return bookRepo.findById(id).get();
     }
 
+    public Map<String, Object> getAllBooks(String name, String isbn, String description, String publisher, Pageable paging) {
+        Page<Book> booksPage = bookRepo.getFilterBooks(name, isbn, description, publisher, paging);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("books", booksPage.getContent());
+        response.put("currentPage", booksPage.getNumber());
+        response.put("totalItems", booksPage.getTotalElements());
+        response.put("totalPages", booksPage.getTotalPages());
+
+        return response;
+    }
 }
